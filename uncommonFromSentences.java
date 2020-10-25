19acd172a9b83cd2f66828435245412e308408d9
kyleyiyang <kyleyiyang@gmail.com>
2020-10-24 23:37:27 -0400

    Update uncommonFromSentences.java

--- uncommonFromSentences.java
+++ uncommonFromSentences.java
@@ -1,86 +1,61 @@
 class Solution {
     public String[] uncommonFromSentences(String A, String B) {
         String D = A+" "+B;
-        int n=D.indexOf(" ");
-        List<String> s=new ArrayList<>();
-        while (n>=0) {
-            String sub = D.substring(0,n);
-            String rest = D.substring(n+1);
+        String out="";
+        //int n=D.indexOf(" ");
+        //List<String> s=new ArrayList<>();
+        out = recursion(D,out);
+        System.out.println("out="+out);
+        return out.split(" ");
+    }
+    public static String recursion(String i, String o) {
+        int n = i.indexOf(" ");
+       // System.out.println("n="+n);
+       // System.out.println("i="+i);
+        if (n==-1) {
+            o=o+i+" ";
+           // System.out.println("o="+o);
+            return o;
+        } else {
+        String sub = i.substring(0,n);
+        String rest = i.substring(n+1);
             System.out.println("sub="+sub+"; rest="+rest);
+        if (!rest.contains(sub)) {
+            o=o+sub+" ";
             
+        }
+        while (rest.contains(sub)) {
             if (sub.equals(rest)) {
-                break;
+                return o;
             }
-            if (rest.indexOf(" ")==-1) {
-                s.add(sub);
-                if (rest!="") {
-                    s.add(rest);
-                }
-                break;
-            } //else {
-                if (!rest.contains(" "+sub)) { // || rest.indexOf(" ")==-1 ) {
-                    s.add(sub);
-                    System.out.println("!rest.contains(sub)="+sub);
-                } else {
-            //if (rest.indexOf(" ")!=-1) {
-                    //String t_sub = " " + sub + " ";
-                    while (rest.contains(sub)) {
-                        if (sub.equals(rest)) {
-                            break;
-                        }
-                        int m = rest.indexOf(sub);
-                        System.out.println("m="+m+"; rest="+rest+"; sub="+sub);
-                        if (m==0) {
-                            if (rest.contains(sub+" ")) {
-                                rest = rest.substring(m+sub.length()+1);
-                            }
-                        } else {
-                            if (rest.contains(" "+sub) && m+sub.length()+1<rest.length()) {
-                                rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
-                            } else {
-                                break;
-                               // rest=rest.substring(0,m-1);
-                            }
-                        }
-                        /*if (m+sub.length()+1<rest.length()) {
-                            rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
-                        } else {
-                            rest=rest.substring(0,m-1);
-                        }*/
-                    }
-                    
-                    
-                    
-                    
-               // while (rest.contains(sub) && rest.substring() {
-                    
-                  //  System.out.println("m="+m+"; rest="+rest+"; sub="+sub);
-                    //if (rest.indexOf(" ")!=-1 && rest.substring(m,m+rest.indexOf(" ")).equals(sub)) {
-                      //  if (m+sub.length()+1<rest.length()) {
-                         //   rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
-                      // } else {
-                      //      rest=rest.substring(0,m-1);
-                     //   }
-                    
+            int z = rest.indexOf(sub);
             
+            if (rest.contains(" "+sub+" ")) {
                 
-                   // } //else {
-                            //rest=rest.substring(0,m-1);
-                        //}
-                    //}
-                //} 
-                //}
-            }
-            D=rest;
-            n=D.indexOf(" ");
-            if (n==-1 && D!="") {
-                s.add(rest);
+                rest = rest.substring(0,z)+rest.substring(z+sub.length()+1);
+            } else if (rest.contains(sub+" ")) {
+                if (z==0) {
+                rest = rest.substring(sub.length()+1);
+                } else {
+                    //Char c=' ';
+                    if (rest.charAt(z-1) != ' ') {
+                        o=o+sub+" ";
+                        break;
+                    }
+                }
+            } else if (rest.contains(" "+sub) && z+sub.length()==rest.length()) {
+                rest = rest.substring(0, z-1);
+            } else {
+                o=o+sub+" ";
+                System.out.println(rest.contains(sub+' '));
+              System.out.println("o="+o);
+                break;
             }
         }
-        String[] out = new String[s.size()];
-        for (int j=0;j<s.size();j++) {
-            out[j] = s.get(j);
+        
+        return recursion(rest,o);
         }
-        return out;
+        //System.out.println("last o="+o);
+        //return o;
     }
 }
