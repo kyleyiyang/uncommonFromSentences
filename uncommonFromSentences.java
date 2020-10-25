c7647da3b9ab8398a89a8c745c15079a1cfcf077
kyleyiyang <kyleyiyang@gmail.com>
2020-10-24 22:41:22 -0400

    Update uncommonFromSentences.java

--- uncommonFromSentences.java
+++ uncommonFromSentences.java
@@ -6,25 +6,74 @@ class Solution {
         while (n>=0) {
             String sub = D.substring(0,n);
             String rest = D.substring(n+1);
+            System.out.println("sub="+sub+"; rest="+rest);
+            
             if (sub.equals(rest)) {
                 break;
             }
-            if (!rest.contains(sub) || rest.indexOf(" ")==-1 ) {
+            if (rest.indexOf(" ")==-1) {
                 s.add(sub);
-            }
+                if (rest!="") {
+                    s.add(rest);
+                }
+                break;
+            } //else {
+                if (!rest.contains(" "+sub)) { // || rest.indexOf(" ")==-1 ) {
+                    s.add(sub);
+                    System.out.println("!rest.contains(sub)="+sub);
+                } else {
             //if (rest.indexOf(" ")!=-1) {
-                while (rest.contains(sub)) {
-                    int m = rest.indexOf(sub);
-                    if (m+sub.length()+1<rest.length() && rest.substring(m,m+sub.length()).equals(sub)) {
-                        rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
-                    } else {
-                        rest=rest.substring(0,m-1);
+                    //String t_sub = " " + sub + " ";
+                    while (rest.contains(sub)) {
+                        if (sub.equals(rest)) {
+                            break;
+                        }
+                        int m = rest.indexOf(sub);
+                        System.out.println("m="+m+"; rest="+rest+"; sub="+sub);
+                        if (m==0) {
+                            if (rest.contains(sub+" ")) {
+                                rest = rest.substring(m+sub.length()+1);
+                            }
+                        } else {
+                            if (rest.contains(" "+sub) && m+sub.length()+1<rest.length()) {
+                                rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
+                            } else {
+                                break;
+                               // rest=rest.substring(0,m-1);
+                            }
+                        }
+                        /*if (m+sub.length()+1<rest.length()) {
+                            rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
+                        } else {
+                            rest=rest.substring(0,m-1);
+                        }*/
                     }
-                } 
-            //}
+                    
+                    
+                    
+                    
+               // while (rest.contains(sub) && rest.substring() {
+                    
+                  //  System.out.println("m="+m+"; rest="+rest+"; sub="+sub);
+                    //if (rest.indexOf(" ")!=-1 && rest.substring(m,m+rest.indexOf(" ")).equals(sub)) {
+                      //  if (m+sub.length()+1<rest.length()) {
+                         //   rest=rest.substring(0,m)+rest.substring(m+sub.length()+1);
+                      // } else {
+                      //      rest=rest.substring(0,m-1);
+                     //   }
+                    
+            
+                
+                   // } //else {
+                            //rest=rest.substring(0,m-1);
+                        //}
+                    //}
+                //} 
+                //}
+            }
             D=rest;
             n=D.indexOf(" ");
-            if (n==-1) {
+            if (n==-1 && D!="") {
                 s.add(rest);
             }
         }
