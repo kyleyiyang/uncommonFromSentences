f734c420519a1cd95378dfb0b01b1644b3b68156
kyleyiyang <kyleyiyang@gmail.com>
2020-10-25 14:55:27 -0400

    Update uncommonFromSentences.java

--- uncommonFromSentences.java
+++ uncommonFromSentences.java
@@ -2,11 +2,10 @@ class Solution {
     public String[] uncommonFromSentences(String A, String B) {
         String D = A+" "+B;
         String out="";
-        //int n=D.indexOf(" ");
-        //List<String> s=new ArrayList<>();
         out = recursion(D,out);
-        System.out.println("out="+out);
-        return out.split(" ");
+        String[] result = {};
+        if (out!="") {result=out.split(" ");}
+        return result;
     }
     public static String recursion(String i, String o) {
         int n = i.indexOf(" ");
@@ -16,57 +15,58 @@ class Solution {
         } else {
         String sub = i.substring(0,n);
         String rest = i.substring(n+1);
-            System.out.println("sub="+sub+"; rest="+rest);
-        if (!rest.contains(sub)) {
+        if (rest.indexOf(sub)==-1) {
             o=o+sub+" ";
-            
-        }
-        if (rest.indexOf(' ')==-1 && !sub.equals(rest)) {
+        } else if (rest.indexOf(' ')==-1 && !sub.equals(rest)) {
             o=o+sub+" ";
-        }
-        while (rest.contains(sub)) {
+        } else {
+        int z = rest.indexOf(sub);
+        boolean flag=false;
+        while (rest.indexOf(sub)!=-1 && z>=0) {
             if (sub.equals(rest)) {
                 return o;
             }
-            int z = rest.indexOf(sub);
-            
-            if (rest.contains(" "+sub+" ")) {
+            if (rest.indexOf(" "+sub+" ",z-1)!=-1) {
                 rest = rest.substring(0,z)+rest.substring(z+sub.length()+1);
-            } else if (rest.contains(sub+" ")) {
+                z = rest.indexOf(sub);
+                flag=true;
+            } else if (rest.indexOf(sub+" ",z)!=-1) {
+                flag=true;
                 if (z==0) {
                 rest = rest.substring(sub.length()+1);
+                    z = rest.indexOf(sub);
                 } else {
-                    //Char c=' ';
-                    if (rest.charAt(z-1) != ' ') {
+                    if (rest.charAt(z-1) != ' ' && rest.indexOf(sub,z+1)==-1) {
                         o=o+sub+" ";
                         break;
                     }
+                    z = rest.indexOf(sub,z+1);
                 }
-            } else if (rest.contains(" "+sub)) {
+            } else if (rest.indexOf(" "+sub,z-1)!=-1) {
+                flag=true;
+                
                 if (z+sub.length()==rest.length()) {
                     rest = rest.substring(0, z-1);
+                    z = rest.indexOf(sub);
+                    break;
                 } else {
-                    if (rest.charAt(z+1) != ' ') {
+                    if (rest.charAt(z+1) != ' ' && rest.indexOf(sub,z+1)==-1) {
                         o=o+sub+" ";
                         break;
                     }
+                    z = rest.indexOf(sub,z+1);
                 }
-            } else {break;}
-            /*if (b==false) {
-                o=o+sub+" ";
-                break;
-            }*/
-            //else {
-                //o=o+sub+" ";
-                
-              //System.out.println("o="+o);
-               //break;
-           // }
+            } else {
+                z = rest.indexOf(sub,z+1);
+                if (z==-1 && !flag) {
+                    o=o+sub+" ";
+                    break;
+                }
+            }
+            
         }
-        
+        }
         return recursion(rest,o);
         }
-        //System.out.println("last o="+o);
-        //return o;
     }
 }
